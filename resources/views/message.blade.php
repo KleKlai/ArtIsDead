8d5a935b103ee888dca117ba0ffa806b18b77809
kleklai <mmagallen@klaiklai.tech>
2022-02-23 23:45:43 +0800

    change resource title

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Art is Dead</title>
+    <title>Voices of the People</title>
 
     <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/lux/bootstrap.min.css"
